a6df2c2a12282bfafd81a0da1abd1298522ce663
AlexeyBondar <alexeybondar91@gmail.com>
2021-10-14 16:23:06 +0300

    сщтефшты еуые

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,2 +1,11 @@
 /// <reference types="Cypress"/>
 
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en');
+    cy.contains('Sign in');
+})
+
+it.only('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en');
+    cy.get('footer').contains('Office');
+})
