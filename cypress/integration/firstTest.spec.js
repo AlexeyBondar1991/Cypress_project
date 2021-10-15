81c499cb628d4cf9fa0d6279138620df3366e81c
AlexeyBondar <alexeybondar91@gmail.com>
2021-10-15 09:29:27 +0300

    s

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,2 +1,34 @@
 /// <reference types="Cypress"/>
 
+it('Type', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    .get('[data-qa-node="phone-number"]')
+    .type(1123123123123)
+})
+
+it('Focus', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    .get('[data-qa-node="amount"]')
+    .focus()
+})
+
+it('blur', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    .get('[data-qa-node="amount"]')
+    .focus()
+    .blur()
+})
+
+it('clear', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    .get('[data-qa-node="amount"]')
+    .type(999)
+    .wait(2000)
+    .clear()
+})
+
+it.only('blur', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    .get('[data-qa-node="amount"]')
+    .
+})
